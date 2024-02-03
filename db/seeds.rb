1608ca7df0833c691b4a4016f73bfcb48b6fc1eb
root <root@localhost>
2024-02-03 07:47:36 +0000

    added books to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+  Book.create!([
+    {title: "Clockwork Orange", author: "Anthony Burgess", price:4.59, published_date: Date.new(1962, 3, 3)},
+    {title: "The Poet", author: "Michal Connelly", price:7.99, published_date: Date.new(1996, 10, 17)},
+    {title: "The Devil in the White City", author: "Eric Larson", price:4.29 , published_date: Date.new(2003, 2, 11)},
+    {title: "The Great Gatsby", author: "F. Scott Fitzgerald", price:5.85 , published_date: Date.new(1925,4,10)},
+    {title: "The Way of Kings", author: "Brandon Sanderson", price:9.89 , published_date: Date.new(2010,8,31)}
+  ])
